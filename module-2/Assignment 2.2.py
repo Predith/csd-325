9bcb507f02deb74c11680dec360c7a240e34db29
Kris Kuenning <krkuenning@gmail.com>
2025-03-23 20:21:21 -0700

    Pushing 03/23/2025

---
+++ module-2/Assignment 2.2.py
@@ -0,0 +1,17 @@
+# Function to calculate factorial
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+try:
+    number = int(input("Enter a number to calculate its factorial: "))
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
+    exit()
+
+# Main program
+number = int(input("Enter a number to calculate its factorial: "))
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
